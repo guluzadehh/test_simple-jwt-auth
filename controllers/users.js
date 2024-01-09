3bbb7433020057ba761d849eb7b940033641229e
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-09 20:09:24 +0400

    Fix log msg and return response in controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
 const login = async (req, res, next) => {
   console.log("Login");
+  res.send();
 };
 
 const register = async (req, res, next) => {
   console.log("Register");
+  res.send();
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.use(require('./middlewares/errorHandler'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  logger.info(`Server is listenin on port ${PORT}`);
+  logger.info(`Server is listening on port ${PORT}`);
 })

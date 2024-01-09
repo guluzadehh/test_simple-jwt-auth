97f464aed02c942ee19202c565335ac6b24c7853
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-09 20:03:04 +0400

    Add base controllers

---
+++ controllers/users.js
@@ -0,0 +1,11 @@
+const login = async (req, res, next) => {
+  console.log("Login");
+};
+
+const register = async (req, res, next) => {
+  console.log("Register");
+};
+
+module.exports = {
+  login, register
+}

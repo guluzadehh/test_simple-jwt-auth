8311205319c4f6e77e39ee573e054e92590b54cb
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-10 01:53:21 +0400

    Update user controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,4 @@
-const { User } = require('../models/')
-const errors = require('../utils/errors')
+const userServices = require('../services/user');
 
 const login = async (req, res, next) => {
   console.log("Login");
@@ -7,20 +6,7 @@ const login = async (req, res, next) => {
 };
 
 const register = async (req, res) => {
-  const user = User.build(req.body);
-  await user.validate();
-
-  const { password, conf_password } = req.body;
-
-  if (password !== conf_password) {
-    throw new errors.ValidationError({ conf_password: "Passwords do not match." });
-  }
-
-  const email = req.body.email || "";
-  const exists = await User.findOne({ where: { email } });
-  if (exists) throw new errors.ConflictError('Email is already taken');
-
-  user.save();
+  const user = await userServices.createUser(req.body);
   return res.json(user.toJSON());
 }
 

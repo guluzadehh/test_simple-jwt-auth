58558801a69da2dfda25ea54b63ac493d093fafe
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-09 20:07:23 +0400

    Add controllers to user routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const controllers = require('../controllers/users');
+
 const router = express.Router();
+
+router.post('/login', controllers.login);
+router.post('/register', controllers.register);
 
 module.exports = router;

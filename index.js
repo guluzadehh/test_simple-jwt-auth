7f2d5e83fd7ae2d12c78f0a7a4bf830adf8c5103
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-09 19:47:12 +0400

    Use error handler middleware

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 })
 
+app.use(require('./middlewares/errorHandler'));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   logger.info(`Server is listenin on port ${PORT}`);

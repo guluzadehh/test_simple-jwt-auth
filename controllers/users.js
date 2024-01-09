177078d0ac476162c24190131958f8ecd0fbeca1
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-09 21:11:00 +0400

    Create register controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,35 @@
+const { User } = require('../models/')
+const errors = require('../errors')
+
 const login = async (req, res, next) => {
   console.log("Login");
   res.send();
 };
 
-const register = async (req, res, next) => {
-  console.log("Register");
-  res.send();
-};
+const register = [
+  (req, res, next) => {
+    const { password, conf_password } = req.body;
+
+    if (password !== conf_password) {
+      throw new errors.ValidationError({ conf_password: "Passwords do not match." });
+    }
+
+    next();
+  },
+
+  async (req, res, next) => {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (user) throw new errors.ConflictError('Email is already taken');
+    next();
+  },
+
+  // image check
+
+  async (req, res) => {
+    const user = await User.create(req.body);
+    return res.json(user.toJSON());
+  }
+]
 
 module.exports = {
   login, register

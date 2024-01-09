a9e374ca230ad945816e1caeeda9d0e33b467c10
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-10 03:50:17 +0400

    Add user update controller

---
+++ src/controllers/profiles.js
@@ -0,0 +1,20 @@
+const userServices = require('../services/user');
+
+const listUsers = async (req, res, next) => {
+  const users = await userServices.listUsers();
+  res.json(users);
+}
+
+const detailUser = async (req, res, next) => {
+  const user = await userServices.fetchUserById(req.params.id);
+  res.json(user);
+}
+
+const updateUser = async (req, res, next) => {
+  const user = await userServices.updateUser(req.params.id, req.body);
+  res.json(user);
+}
+
+module.exports = {
+  listUsers, detailUser, updateUser
+}

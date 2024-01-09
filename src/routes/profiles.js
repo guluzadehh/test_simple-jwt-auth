32fe493454320f44767a5dc5b309dfbd710b984f
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-10 03:36:47 +0400

    Create routes for profiles

--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const controllers = require('../controllers/profiles');
+
+router.get('/', controllers.listUsers);
+router.route('/:id')
+  .get(controllers.detailUser)
+  .put(controllers.updateUser);
+
 module.exports = router;

8e9e9a791978ef62c83089d6a213a7e27b1b2265
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-09 19:46:08 +0400

    Add error converter helper function

---
+++ helpers/index.js
@@ -0,0 +1,20 @@
+const errors = require('./errors')
+const logger = require('./config/logger')
+
+function convertToAPIError(err) {
+  if (err.name === 'SequelizeValidationError') {
+    const details = err.errors.reduce((obj, error) => {
+      return { ...obj, [error.path]: error.message }
+    }, {})
+
+    return new errors.ValidationError(details)
+  }
+
+  logger.error(err.message)
+
+  return new errors.InternalError()
+}
+
+module.exports = {
+  convertToAPIError
+}

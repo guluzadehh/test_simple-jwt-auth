d43e8d0d58a206e1bb3df90dc56377f88412837b
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-10 05:52:18 +0400

    Add image saver and image name generator utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
 const errors = require('../utils/errors')
 const logger = require('../config/logger')
 
@@ -15,6 +18,25 @@ function convertToAPIError(err) {
   return new errors.InternalError()
 }
 
+const generateImageName = (originalName) => {
+  return `${crypto.randomBytes(16).toString('hex')}${path.extname(originalName)}`
+}
+
+const saveImage = (imagePath, imageBuffer) => {
+  if (imageBuffer == null) return;
+
+  const uploadDir = path.join(__dirname, '/../uploads/');
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  const out = path.join(uploadDir, imagePath);
+  fs.writeFileSync(out, imageBuffer);
+
+  return out;
+}
+
 module.exports = {
-  convertToAPIError
+  convertToAPIError, saveImage, generateImageName
 }

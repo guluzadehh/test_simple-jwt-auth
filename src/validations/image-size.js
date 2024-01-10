6cc9012e1a07db38012db06f93bb4a1190a64570
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-10 06:14:00 +0400

    Add image validation

---
+++ src/validations/image-size.js
@@ -0,0 +1,8 @@
+const imageValid = (file) => {
+  const maxImageSize = 10 * 1024 * 1024;
+
+  const originalName = file?.originalname;
+  const buffer = file?.buffer;
+
+  return originalName != null && buffer != null && Buffer.byteLength(buffer) <= maxImageSize;
+}

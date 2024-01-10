c5f65d891a5af641b1c56ad1cc9bdf8f5d399c1e
Resul Quluzade <quluzade.resul13@mail.ru>
2024-01-10 04:15:34 +0400

    Configure routes for profile

--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const controllers = require('../controllers/profiles');
+const { isSameUser } = require('../middlewares/userPermission');
 
 router.get('/', controllers.listUsers);
+
+router.use(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  isSameUser
+)
+
 router.route('/:id')
   .get(controllers.detailUser)
   .put(controllers.updateUser);
